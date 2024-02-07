427891672603533f545f850c2fe4e97538661c61
Magnus H <mfh1106@gmail.com>
2024-02-07 14:30:25 +0100

    Init pickle additions

--- worstWindow.py
+++ worstWindow.py
@@ -1,5 +1,14 @@
 # importing tkinter module
+import pickle
 from tkinter import *
+filename = 'betalinger.pk'
+
+# Directory til betalinger.pk filen...
+fodboldtur ={"assets/"}
+
+infile = open(filename,'rb')
+fodboldtur = pickle.load(infile)
+infile.close()
 
 class worstWindowClass:
     def __init__(self, master):
@@ -8,5 +17,11 @@ class worstWindowClass:
         self.worstWindow.title("Bottom 3")
         self.worstWindow.geometry("200x200")
 
-        Label(self.worstWindow, text="De værste betalere").pack()
+
+
+        Label(self.worstWindow, text="De værste betalere")
+        #TODO Fix pickle load / label merge
+        top = sorted(fodboldtur.items(), key=lambda item: item[1])
+        for pair in top[:3]:
+            Label(self.worstWindow, text=f"{pair[0]} Mangler {pair[1]}").pack
 
